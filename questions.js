bcd2d7410c2f2b6f626ee1a141da8b86fa3ad709
Trestar945 <150948015+Trestar945@users.noreply.github.com>
2023-11-30 18:49:38 -0500

    Added new files

---
+++ questions.js
@@ -0,0 +1,332 @@
+let questions = [
+    {
+        numb: 1,
+        questions: "What does Cru stand for?",
+        answer: "C. Campus Crusade for Christ",
+        options: [
+            "A. College Rival Union",
+            "B. Chrisitian Righteous Unity",
+            "C. Campus Crusade for Christ",
+            "D. College Rock Unit"
+        ]
+    },
+    {
+        numb: 2,
+        questions: "Who was the founder of Cru?",
+        answer: "B. Bill Bright",
+        options: [
+            "A. Benjamin Franklin",
+            "B. Bill Bright",
+            "C. Bob Ross",
+            "D. Bill Nye"
+        ]
+    },
+    {
+        numb: 3,
+        questions: "What is Cru specifically passionate about?",
+        answer: "A. Connecting others to Jesus Christ.",
+        options: [
+            "A. Connecting others to Jesus Christ.",
+            "B. Convincing everyone to get baptized",
+            "C. Chick-fil-a",
+            "D. Spider-man"
+        ]
+    },
+    {
+        numb: 4,
+        questions: "True Definition of Successful Evangelism:",
+        answer: "D. Taking the initiative to share the gospel and leaving the results to God.",
+        options: [
+            "A. Objecting God's calling",
+            "B. Inviting people to Cook Out",
+            "C. Making everyone you share the gospel to become Christian",
+            "D. Taking the initiative to share the gospel and leaving the results to God."
+        ]
+    },
+    {
+        numb: 5,
+        questions: "When was Cru Founded?",
+        answer: "C. 1951",
+        options: [
+            "A. 1947",
+            "B. 1949",
+            "C. 1951",
+            "D. 1953"
+        ]
+    },
+    {
+        numb: 6,
+        questions: "What Armor element is NOT part of the Armor of God",
+        answer: "A. Back armor plate of Wisdom.",
+        options: [
+            "A. Back armor plate of Wisdom.",
+            "B. Belt of Truth",
+            "C. Helmet of Salvation",
+            "D. Shield of Faith."
+        ]
+    },
+    {
+        numb: 7,
+        questions: "To be born again and enter salvation, you must___",
+        answer: "B. Confess that Jesus is your Lord and Savior and have faith.",
+        options: [
+            "A. Not sinning.",
+            "B. Confess that Jesus is your Lord and Savior and have faith.",
+            "C. Going to Church every Sunday",
+            "D. Getting Baptized."
+        ]
+    },
+    {
+        numb: 8,
+        questions: "After Crucifixion, Jesus rose how many days",
+        answer: "B. 3",
+        options: [
+            "A. 1",
+            "B. 3",
+            "C. 7",
+            "D. 10"
+        ]
+    },
+    {
+        numb: 9,
+        questions: "Finish the phrase: for the wages of sin is death, but the gift of God is eternal life in ____ ",
+        answer: "D. Jesus Christ our Lord",
+        options: [
+            "A. Yourself",
+            "B. Our planet Earth",
+            "C. The Sun",
+            "D. Jesus Christ our Lord"
+        ]
+    },
+    {
+        numb: 10,
+        questions: "What was Jesus FIRST miracle?",
+        answer: "B. Turning water into wine",
+        options: [
+            "A. Feeding the 5000",
+            "B. Turning water into wine",
+            "C. Giving sight to the born blind man",
+            "D. Casting demons of people"
+        ]
+    },
+    {
+        numb: 11,
+        questions: "Which was one temptation that Satan casted at Jesus during his 40-day journey in the wilderness?",
+        answer: "D. Telling him to turn stones into bread",
+        options: [
+            "A. Casting A tsunami",
+            "B. Throwing lava at him",
+            "C. Summoning 30 lions to kill him",
+            "D. Telling him to turn stones into bread",
+        ]
+    },
+    {
+        numb: 12,
+        questions: "Who were the very first two people created by God?",
+        answer: "C. Adam and Eve",
+        options: [
+            "A. Adam and Steve",
+            "B. Madam and Eve",
+            "C. Adam and Eve",
+            "D. Adam and Jesus",
+        ]
+    },
+    {
+        numb: 13,
+        questions: "The three critical aspects that the Cru ministry do to people",
+        answer: "B. Win, Build, and Send",
+        options: [
+            "A. Win, Build, and Marry",
+            "B. Win, Build, and Send",
+            "C. Worry, Fear, and Fight ",
+            "D. Hate, Disrespect, and Traumatize",
+        ]
+    },
+    {
+        numb: 14,
+        questions: "What political party do you must follow to enter salvation?",
+        answer:   "D. It's a trick question",
+        options: [
+            "A. Democracy",
+            "B. Republic",
+            "C. Communism",
+            "D. It's a trick question",
+        ]
+    },
+    {
+        numb: 15,
+        questions: "How many commandments are there in TOTAL?",
+        answer:   "C. 613",
+        options: [
+            "A. 5",
+            "B. 10",
+            "C. 613",
+            "D. 1085",
+        ]
+    },
+    {
+        numb: 16,
+        questions: "What did God created on the 4th day?",
+        answer:   "B. The sun, moon and stars",
+        options: [
+            "A. Birds and sea creatures",
+            "B. The sun, moon and stars",
+            "C. Ground and plants",
+            "D. Humans and land animals",
+        ]
+    },
+    {
+        numb: 17,
+        questions: "What did God created on the 7th day?",
+        answer:   "D. Nothing Bro!!!/ He rested",
+        options: [
+            "A. Jesus",
+            "B. People",
+            "C. The Earth",
+            "D. Nothing Bro!!!/ He rested",
+        ]
+    },
+    {
+        numb: 18,
+        questions: "What did God created on the 6th day?",
+        answer:   "D. Humans and land animals",
+        options: [
+            "A. Birds and sea creatures",
+            "B. The sun, moon and stars",
+            "C. Ground and plants",
+            "D. Humans and land animals",
+        ]
+    },
+    {
+        numb: 19,
+        questions: "What did God created on the 3rd day?",
+        answer:   "C. Ground and plants",
+        options: [
+            "A. Birds and sea creatures",
+            "B. The sun, moon and stars",
+            "C. Ground and plants",
+            "D. Humans and land animals",
+        ]
+    },
+    {
+        numb: 20,
+        questions: "What did God created on the 5th day?",
+        answer:   "A. Birds and sea creatures",
+        options: [
+            "A. Birds and sea creatures",
+            "B. The sun, moon and stars",
+            "C. Ground and plants",
+            "D. Humans and land animals",
+        ]
+    },
+    {
+        numb: 21,
+        questions: "What did God created on the 1st day?",
+        answer:   "D. Light",
+        options: [
+            "A. The Land creatures",
+            "B. Reptiles",
+            "C. Jesus",
+            "D. Light",
+        ]
+    },
+    {
+        numb: 22,
+        questions: "What did God created on the 2nd day?",
+        answer:   "B. Atmosphere",
+        options: [
+            "A. Light",
+            "B. Atmosphere",
+            "C. Sand",
+            "D. Sound",
+        ]
+    },
+    {
+        numb: 23,
+        questions: "Where did Cru started at?",
+        answer:   "C. University of California",
+        options: [
+            "A. Liberty University",
+            "B. Los Angeles University",
+            "C. University of California",
+            "D. Campbell University",
+        ]
+    },
+    {
+        numb: 24,
+        questions: "What did Jesus do to water in the wedding at Cana?",
+        answer:    "A. Turned it into wine",
+        options: [
+            "A. Turned it into wine",
+            "B. Turned it into Bud-Light",
+            "C. Poisoned it",
+            "D. Transformed it into lava",
+        ]
+    },
+    {
+        numb: 25,
+        questions: "A  story used to illustrate a moral or spiritual lesson is known as___",
+        answer:   "D. A Parable",
+        options: [
+            "A. The Bible",
+            "B. A Commandment",
+            "C. A Disciple",
+            "D. A Parable",
+        ]
+    },
+    {
+        numb: 30,
+        questions: "Finish the phrase: Go and make ___ of all nations",
+        answer:   "C. Disciples",
+        options: [
+            "A. Friends",
+            "B. Boyfriends/Girlfriends",
+            "C. Disciples",
+            "D. Enemies",
+        ]
+    },
+    {
+        numb: 27,
+        questions: "A  Finish the phrase: baptizing them in the Name of the __, __, and ___ ",
+        answer:   "B. Father, Son, Holy Spirit",
+        options: [
+            "A. Sister, Son, Holy Goat",
+            "B. Father, Son, Holy Spirit",
+            "C. Father, Son, Mother",
+            "D. Water, Son, Holy Spirit ",
+        ]
+    },
+    {
+        numb: 28,
+        questions: "Which Disciple Betrayed Jesus?",
+        answer:   "D. Judas",
+        options: [
+            "A. Paul",
+            "B. Joseph",
+            "C. Peter",
+            "D. Judas",
+        ]
+    },
+    {
+        numb: 29,
+        questions: "Which Disciple Denied Jesus 3 times?",
+        answer:   "C. Peter",
+        options: [
+            "A. Paul",
+            "B. Joseph",
+            "C. Peter",
+            "D. Judas",
+        ]
+    },
+    {
+        numb: 30,
+        questions: "How many disciples were accompanying Jesus",
+        answer:   "A. 12",
+        options: [
+            "A. 12",
+            "B. 15",
+            "C. 21",
+            "D. 45",
+        ]
+    },
+];
